f8673f07467fdc4ee72c0b55411dd952d630962f
MAnikanta <21000cseh@gmail.com>
2023-07-14 16:47:05 +0530

    changes

--- pp/pp/views.py
+++ pp/pp/views.py
@@ -1,3 +1,6 @@
-#hai mahikiran
-#hai manikanta
-#hello 
+
+# #hello
+#
+# This command switches your local repository to the branch1 branch, allowing you to access and work with its data.
+#
+# Now, you have the contents of the branch1 branch from your GitHub repository on your laptop. You can make changes, commit them locally, and push them back to the remote repository when you're ready.
